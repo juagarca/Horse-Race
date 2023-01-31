0993d0c550152accf403c7250a510d2bee28d11a
Juan Garcia <juagarca@gmail.com>
2023-01-31 21:00:47 +0000

    Initial commit

---
+++ lib/interface.rb
@@ -0,0 +1,12 @@
+balance = 100
+
+# 1. Print welcome and the horses names
+
+# 2. Get user's bet
+# 2.a. Ask the user for the horse they want to win
+
+# 3. Run the race 🐴
+# 3.a If the user won, increase £50
+# 3.b If the user lost, decrease £10
+
+# 4. Print results
